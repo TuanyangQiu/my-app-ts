263c2bae026e4108e28350b861dd3cb9f7f6cc24
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-03 10:04:11 +1200

    fetch data through api instead of local mock data

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,50 @@ import styles from './App.module.css';
 import ShoppingCart from './components/ShoppingCart';
 
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} alt="logo" className={styles.appLogo} />
-        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字一定要很长</h1>
-      </div>
+interface Props { }
 
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {
-          robots.map(r => (<Robot id={r.id} name={r.name} email={r.email} />))
-        }
+interface State {
+
+  robotGallery: any
+
+}
+
+class App extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+
+    super(props);
+
+    this.state = {
+      robotGallery: []
+    };
+  }
+
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => this.setState({ robotGallery: data }));
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} alt="logo" className={styles.appLogo} />
+          <h1>罗伯特机器人炫酷吊炸天online购物平台的名字一定要很长</h1>
+        </div>
+
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {
+            this.state.robotGallery.map(r => (<Robot id={r.id} name={r.name} email={r.email} />))
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
+
 }
 
 export default App;
