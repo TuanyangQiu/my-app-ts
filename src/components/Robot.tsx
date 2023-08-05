3bda9f8615c10d953952835a8ef36d61ef554eed
TuanyangQiu <qiutuanyang@gmail.com>
2023-08-06 11:46:02 +1200

    get data from context through useContext hooks

--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Styles from './Robot.module.css';
 import { appContext } from '../index'
 
@@ -11,18 +11,26 @@ interface RobotProps {
 
 
 const Robot: React.FC<RobotProps> = ({ name, id, email }) => {
+    const context = useContext(appContext);//useContext hooks
     return (
 
-        //get data from context
-        <appContext.Consumer>{(value) => {
-            return <div className={Styles.cardContainer}>
-                <img src={`https://robohash.org/${id}`} alt="robot" />
-                <h2>{name}</h2>
-                <p>{email}</p>
-                <p>Author: {value.userName}</p>
-            </div>
+        //1: get data from context through context consumer
+        // <appContext.Consumer>{(value) => {
+        //     return <div className={Styles.cardContainer}>
+        //         <img src={`https://robohash.org/${id}`} alt="robot" />
+        //         <h2>{name}</h2>
+        //         <p>{email}</p>
+        //         <p>Author: {value.userName}</p>
+        //     </div>
+        // }}</appContext.Consumer>
 
-        }}</appContext.Consumer>
+        //2: get data from context through useContext hooks, which is easier
+        <div className={Styles.cardContainer}>
+            <img src={`https://robohash.org/${id}`} alt="robot" />
+            <h2>{name}</h2>
+            <p>{email}</p>
+            <p>Author: {context.userName}</p>
+        </div>
 
     );
 };
